0dcac46480a64357f645f793d67374b2e0ded4e3
Julia March <101819212+juliamrch@users.noreply.github.com>
2024-05-13 12:06:27 +0200

    chore: add Cellar storage

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,26 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            accessKeyId: env('CELLAR_ADDON_KEY_ID'),
+            secretAccessKey: env('CELLAR_ADDON_KEY_SECRET'),
+            //region: env('AWS_REGION'),
+            params: {
+              Bucket: env('CELLAR_BUCKET'),
+            },
+          }
+        },
+        // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
+        actionOptions: {
+          upload: {
+            ACL: null
+          },
+          uploadStream: {
+            ACL: null
+          },
+        }
+      },
+    }
+  });
