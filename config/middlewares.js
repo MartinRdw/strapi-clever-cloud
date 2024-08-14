d2eafada46091587fa83a208111228636d9ba51e
Martin Riedweg <martin.riedweg@gmail.com>
2024-08-15 00:02:27 +0200

    Fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,36 @@
-module.exports = [
-  'strapi::logger',
+export default [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `https://${process.env.CELLAR_BUCKET}.${process.env.CELLAR_ADDON_HOST}`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `https://${process.env.CELLAR_BUCKET}.${process.env.CELLAR_ADDON_HOST}`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
