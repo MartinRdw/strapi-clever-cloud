9324c5bc9a68bffe6bd859d6c1dd83bf930e5db2
Martin Riedweg <martin.riedweg@gmail.com>
2024-08-14 17:48:14 +0200

    Fix

--- config/database.js
+++ config/database.js
@@ -1,39 +1,28 @@
 const path = require('path');
 
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env("POSTGRESQL_ADDON_URI"),
-        host: env("POSTGRESQL_ADDON_HOST", "localhost"),
-        port: env.int("POSTGRESQL_ADDON_PORT", 5432),
-        database: env("POSTGRESQL_ADDON_DB", "strapi"),
-        user: env("POSTGRESQL_ADDON_USER", "strapi"),
-        password: env("POSTGRESQL_ADDON_PASSWORD", "strapi"),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    }
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      connectionString: env("POSTGRESQL_ADDON_URI"),
+      host: env("POSTGRESQL_ADDON_HOST", "localhost"),
+      port: env.int("POSTGRESQL_ADDON_PORT", 5432),
+      database: env("POSTGRESQL_ADDON_DB", "strapi"),
+      user: env("POSTGRESQL_ADDON_USER", "strapi"),
+      password: env("POSTGRESQL_ADDON_PASSWORD", "strapi"),
+      ssl: env.bool('DATABASE_SSL', false) && {
+        key: env('DATABASE_SSL_KEY', undefined),
+        cert: env('DATABASE_SSL_CERT', undefined),
+        ca: env('DATABASE_SSL_CA', undefined),
+        capath: env('DATABASE_SSL_CAPATH', undefined),
+        cipher: env('DATABASE_SSL_CIPHER', undefined),
+        rejectUnauthorized: env.bool(
+          'DATABASE_SSL_REJECT_UNAUTHORIZED',
+          true
+        ),
+      },
+      schema: env('DATABASE_SCHEMA', 'public'),
     },
-  };
-};
+    pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+  },
+});
